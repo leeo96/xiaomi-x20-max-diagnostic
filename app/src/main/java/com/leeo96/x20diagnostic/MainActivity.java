3b0ec3a3e1f5a95bfb60020e364904f5f8eed577
Leonardo Trinta <32195000+leeo96@users.noreply.github.com>
2026-08-31 21:01:51 -0300

    Use Xiaomi Cloud RC4 body-post client

--- app/src/main/java/com/leeo96/x20diagnostic/MainActivity.java
+++ app/src/main/java/com/leeo96/x20diagnostic/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends Activity {
                     android.webkit.CookieManager wc = android.webkit.CookieManager.getInstance();
                     String accountCookies = wc.getCookie("https://account.xiaomi.com");
                     String stsCookies = wc.getCookie("https://sts.api.io.mi.com");
-                    BrowserSessionCloudClientV2 client = new BrowserSessionCloudClientV2();
+                    BrowserSessionCloudClientV3 client = new BrowserSessionCloudClientV3();
                     deliver(requestId, client.fetchFromBrowserSession(accountCookies, stsCookies));
                 } catch (Throwable t) {
                     deliver(requestId, errorPayload(t));
